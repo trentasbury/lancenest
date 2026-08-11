bed211e700c6515d0c6d73fcc830805a1e6a1dcb
trentasbury <trent.asbury18@gmail.com>
2026-08-11 13:10:11 -0700

    Update ApplyBox.js

--- app/listings/[id]/ApplyBox.js
+++ app/listings/[id]/ApplyBox.js
@@ -13,7 +13,8 @@ export default function ApplyBox({ listingId, clientId }) {
 
   useEffect(() => {
     async function load() {
-      const { data: { user } } = await supabase.auth.getUser();
+      const { data: { session } } = await supabase.auth.getSession();
+      const user = session?.user;
       setUser(user);
       if (user) {
         const { data } = await supabase
