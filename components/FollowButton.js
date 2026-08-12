f8b8e2bf71ecb75d32fec07d953b3631708d4cd3
trentasbury <trent.asbury18@gmail.com>
2026-08-12 11:46:45 -0700

    Update FollowButton.js

--- components/FollowButton.js
+++ components/FollowButton.js
@@ -11,7 +11,8 @@ export default function FollowButton({ targetId }) {
 
   useEffect(() => {
     async function load() {
-      const { data: { user } } = await supabase.auth.getUser();
+      const { data: { session } } = await supabase.auth.getSession();
+      const user = session?.user;
       if (!user || user.id === targetId) {
         setChecking(false);
         return;
